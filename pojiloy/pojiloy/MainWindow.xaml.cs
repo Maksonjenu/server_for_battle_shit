2f8c1a5ae6cf1f8a849f3ffb7514ed8b07050d5e
Maksonjenu <genius690@mail.ru>
2019-05-13 00:22:03 +0700

    new desighn + server part

--- pojiloy/pojiloy/MainWindow.xaml.cs
+++ pojiloy/pojiloy/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,21 +23,205 @@ namespace pojiloy
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+        public string jjjj = "";
+       static public int cletka = 0 ;
+        const int port = 25565;
+        static TcpListener listener;
+        bool isWorking = true;
+        Thread myThread1;
+
+
+        public void Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+            int n = MainWindow.cletka;
+            //получение значения лежащего в Tag
+            // int n = (int)((Button)sender).Tag;
+
+            //установка фона нажатой кнопки, цвета и размера шрифта
+            ((Button)sender).Background = Brushes.Red;
+            ((Button)sender).Foreground = Brushes.Black;
+            ((Button)sender).FontSize = 8;
+            //запись в нажатую кнопку её номера
+            ((Button)sender).Content = n.ToString();
+            cletka++;
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
-            
+            ugr.Rows = 10;
+            ugr.Columns = 10;
+
+
+            //указываются размеры сетки (число ячеек * (размер кнопки в ячейки + толщина её границ))
+            // ugr.Width = 10 * (40 + 4);
+            // ugr.Height = 10 * (40 + 4);
+            //толщина границ сетки
+            // ugr.Margin = new Thickness(-456, 10, 458, -2);
+            for (int i = 0; i < 100; i++)
+            {
+                //создание кнопки
+                Button btn = new Button();
+                //запись номера кнопки
+                btn.Tag = i + 1;
+                //установка размеров кнопки
+                btn.Width = 40;
+                btn.Height = 40;
+                //текст на кнопке
+                btn.Content = i;
+                //толщина границ кнопки
+                btn.Margin = new Thickness(1);
+                //при нажатии кнопки, будет вызываться метод Btn_Click
+                btn.Click += Btn_Click;
+                //добавление кнопки в сетку
+                ugr.Children.Add(btn);
+
+            }
+            ugr1.Rows = 10;
+            ugr1.Columns = 10;
+            //указываются размеры сетки (число ячеек * (размер кнопки в ячейки + толщина её границ))
+            // ugr1.Width = 10 * (40 + 4);
+            // ugr1.Height = 10 * (40 + 4);
+            //толщина границ сетки
+            //  ugr1.Margin = new Thickness(2, 2, 2, 2);
+            for (int i = 0; i < 100; i++)
+            {
+                //создание кнопки
+                Button btn1 = new Button();
+                //запись номера кнопки
+                btn1.Tag = i + 1;
+                //установка размеров кнопки
+                btn1.Width = 40;
+                btn1.Height = 40;
+                //текст на кнопке
+                btn1.Content = i;
+                //толщина границ кнопки
+                btn1.Margin = new Thickness(1);
+                //при нажатии кнопки, будет вызываться метод Btn_Click
+                btn1.Click += Btn_Click;
+                //добавление кнопки в сетку
+                ugr1.Children.Add(btn1);
+
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            player1 ww1 = new player1();
-            ww1.Owner = this;
-            ww1.Show();
-            player2 ww2 = new player2();
-            ww2.Owner = this;
-            ww2.Show();
+          //  player1 ww1 = new player1();
+          //  ww1.Owner = this;
+          //  ww1.Show();
+
+
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+            listener.Start();
+            myThread1 = new Thread(new ThreadStart(Count1));
+            myThread1.Start();
+        }
+        public void Count1()
+        {
+            try
+            {
+                while (isWorking)
+                {
+                    Dispatcher.BeginInvoke(new Action(() => status.Foreground = Brushes.Green));
+                    Dispatcher.BeginInvoke(new Action(() => status.Content = ("server started")));
+
+                    TcpClient client = listener.AcceptTcpClient();
+
+                    Thread clientThread = new Thread(() => Process(client));
+                    clientThread.Start();
+                }
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("vse ochen ploho");
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+        public void Process(TcpClient tcpClient)
+        {
+            TcpClient client = tcpClient;
+            NetworkStream stream = null;
+
+            try
+            {
+              
+                stream = client.GetStream();
+               
+
+
+                //цикл обработки сообщений
+                while (isWorking)
+                {
+                    byte[] data = new byte[64];
+
+                    //объект, для формирования строк
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    //до тех пор, пока в потоке есть данные
+                    do
+                    {
+                        //из потока считываются 64 байта и записываются в data
+                        bytes = stream.Read(data, 0, data.Length);
+                        //из считанных данных формируется строка
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+
+                     jjjj = builder.ToString();
+
+                    Dispatcher.BeginInvoke(new Action(() => tags.Content = jjjj));
+
+
+                    //преобразование сообщения в набор байт
+                    // data = Encoding.Unicode.GetBytes(message);
+                    //отправка сообщения обратно клиенту
+                    //stream.Write(data, 0, data.Length);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("danger, danger, high voltage");
+                //Console.WriteLine(ex.Message);
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+            finally
+            {
+                //освобождение ресурсов при завершении сеанса
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            listener.Stop();
+        }
+
+        private void Cifora_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            listener.Stop();
         }
     }
 }
--- pojiloy/pojiloy/player1.xaml.cs
+++ pojiloy/pojiloy/player1.xaml.cs
@@ -19,27 +19,35 @@ namespace pojiloy
     /// </summary>
     public partial class player1 : Window
     {
-        private void Btn_Click(object sender, RoutedEventArgs e)
+     
+        public void Btn_Click(object sender, RoutedEventArgs e)
         {
+          
+            int n = MainWindow.cletka;
             //получение значения лежащего в Tag
-            int n = (int)((Button)sender).Tag;
+           // int n = (int)((Button)sender).Tag;
+        
             //установка фона нажатой кнопки, цвета и размера шрифта
-            ((Button)sender).Background = Brushes.White;
-            ((Button)sender).Foreground = Brushes.Red;
-            ((Button)sender).FontSize = 9;
+            ((Button)sender).Background = Brushes.Red;
+            ((Button)sender).Foreground = Brushes.Black;
+            ((Button)sender).FontSize = 8;
             //запись в нажатую кнопку её номера
             ((Button)sender).Content = n.ToString();
         }
+     
         public player1()
         {
+          
             InitializeComponent();
             ugr.Rows = 10;
             ugr.Columns = 10;
+            
+            
             //указываются размеры сетки (число ячеек * (размер кнопки в ячейки + толщина её границ))
-            ugr.Width = 10 * (40 + 4);
-            ugr.Height = 10 * (40 + 4);
+            // ugr.Width = 10 * (40 + 4);
+            // ugr.Height = 10 * (40 + 4);
             //толщина границ сетки
-            ugr.Margin = new Thickness(2, 2, 2, 2);
+            // ugr.Margin = new Thickness(-456, 10, 458, -2);
             for (int i = 0; i < 100; i++)
             {
                 //создание кнопки
@@ -50,14 +58,42 @@ namespace pojiloy
                 btn.Width = 40;
                 btn.Height = 40;
                 //текст на кнопке
-                btn.Content = " ";
+                btn.Content = i;
                 //толщина границ кнопки
                 btn.Margin = new Thickness(1);
                 //при нажатии кнопки, будет вызываться метод Btn_Click
                 btn.Click += Btn_Click;
                 //добавление кнопки в сетку
                 ugr.Children.Add(btn);
+               
+            }
+            ugr1.Rows = 10;
+            ugr1.Columns = 10;
+            //указываются размеры сетки (число ячеек * (размер кнопки в ячейки + толщина её границ))
+           // ugr1.Width = 10 * (40 + 4);
+           // ugr1.Height = 10 * (40 + 4);
+            //толщина границ сетки
+          //  ugr1.Margin = new Thickness(2, 2, 2, 2);
+            for (int i = 0; i < 100; i++)
+            {
+                //создание кнопки
+                Button btn1 = new Button();
+                //запись номера кнопки
+                btn1.Tag = i + 1;
+                //установка размеров кнопки
+                btn1.Width = 40;
+                btn1.Height = 40;
+                //текст на кнопке
+                btn1.Content = i;
+                //толщина границ кнопки
+                btn1.Margin = new Thickness(1);
+                //при нажатии кнопки, будет вызываться метод Btn_Click
+                btn1.Click += Btn_Click;
+                //добавление кнопки в сетку
+                ugr1.Children.Add(btn1);
+
             }
         }
-    }
+  
+}
 }
